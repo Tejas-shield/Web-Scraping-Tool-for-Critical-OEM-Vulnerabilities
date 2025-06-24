77d5a3d9a8265b3414c6156475593547bee295a0
Tejas <alwaysteju2003@gmail.con>
2025-06-24 19:01:00 +0530

    chart updated

--- scanner/views.py
+++ scanner/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Vulnerability
 from django.db.models import Q
-
+from django.db.models import Count
+import json
+from collections import Counter
 def home(request):
     query = request.GET.get('q', '')
     source_filter = request.GET.get('source', '')
@@ -16,11 +18,23 @@ def home(request):
     if source_filter and source_filter.lower() != "all":
         vulnerabilities = vulnerabilities.filter(source__icontains=source_filter)
 
-    sources = Vulnerability.objects.values_list('source', flat=True).distinct()
+    available_sources = Vulnerability.objects.values_list('source', flat=True).distinct()
 
+    # Count each severity level
+    severity_counts = Counter(v.severity or "Unknown" for v in vulnerabilities)
+    # Sort for consistent x-axis
+    severity_order = ["CRITICAL", "HIGH", "MEDIUM", "LOW", "Unknown"]
+    chart_data = {
+        "labels": json.dumps(severity_order),
+        "data": json.dumps([severity_counts.get(sev, 0) for sev in severity_order])
+    }
+
+
+    
     return render(request, 'scanner/home.html', {
         'vulnerabilities': vulnerabilities,
-        'available_sources': sources,
+        'available_sources': available_sources,
         'query': query,
         'selected_source': source_filter,
+        'chart_data': chart_data
     })
